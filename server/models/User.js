393f58883747da2ff206e9a1c8d6c3128281414b
Allison Higginbotham <allison@fortyau.com>
2018-05-20 20:04:04 -0500

    Add mongoose model for User and use it in the passport service

---
+++ server/models/User.js
@@ -0,0 +1,9 @@
+// Mongoose model class
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleId: String
+});
+
+mongoose.model('users', userSchema);
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,12 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+// our model class - gives us a handle
+// to underlying collection that exists
+// in our MongoDB
+const User = mongoose.model('users');
 
 // tell passport how to make use of the GoogleStrategy
 passport.use(
@@ -11,9 +17,15 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('refreshToken', refreshToken);
-      console.log('accessToken', accessToken);
-      console.log('profile', profile);
+      // first check to see if we have a user already
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // already have record with profile ID
+        } else {
+          // create new instance of a user & save to db
+          new User({ googleId: profile.id }).save();
+        }
+      });
     }
   )
 );
