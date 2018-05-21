07f9cffbfb51c53e95ce3ab57a50cf177aabe49d
Allison Higginbotham <allison@fortyau.com>
2018-05-21 12:55:41 -0500

    Install cookie-session and tell passport to use cookies

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
+// Initial application setup happens in here
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -7,8 +10,19 @@ require('./services/passport');
 // instruct mongoose to connect to MongoDB
 mongoose.connect(keys.mongoURI);
 
-
 const app = express();
+
+// tell express to make use of cookies
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+// tell passport to use cookies to manage authentication
+app.use(passport.initialize());
+app.use(passport.session());
 
 // when we require the authRoutes files -- it returns a function
 // so here we are immediately invoking this function and passing
